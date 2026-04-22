a866493c6646a966da2d6f51f5f8a2d69e96b4e0
Anas Syed <anasyd2002@gmail.com>
2026-04-22 13:17:56 +0100

    feat(server): add MongoDB connection

---
+++ server/src/db.ts
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+import { config } from "./config.js";
+import { logger } from "./logger.js";
+
+export const mongo = new MongoClient(config.MONGODB_URI);
+
+export async function connectDb(): Promise<void> {
+  await mongo.connect();
+  await mongo.db().command({ ping: 1 });
+  logger.info(
+    { uri: config.MONGODB_URI.replace(/\/\/[^@]+@/, "//***@") },
+    "mongo connected",
+  );
+}
+
+export async function disconnectDb(): Promise<void> {
+  await mongo.close();
+  logger.info("mongo disconnected");
+}
