84e4443310a9b4cf725b393d54d3845704264622
Anas Syed <anasyd2002@gmail.com>
2026-04-30 20:50:51 +0100

    feat: add tracking pixel via proxied link for cross-domain instance detection

--- web/src/components/perf-observer.tsx
+++ web/src/components/perf-observer.tsx
@@ -4,11 +4,15 @@ import Script from "next/script";
 
 export function PerfObserver() {
   return (
-    <Script
-      src="/u/script.js"
-      data-website-id="216497cd-cf5b-46f6-82ff-be37ddfe35d3"
-      data-host-url="/u"
-      strategy="afterInteractive"
-    />
+    <>
+      <Script
+        src="/u/script.js"
+        data-website-id="216497cd-cf5b-46f6-82ff-be37ddfe35d3"
+        data-host-url="/u"
+        strategy="afterInteractive"
+      />
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/u/q/TJjk1M7XB" alt="" width={1} height={1} style={{ position: "absolute", left: -9999 }} />
+    </>
   );
 }
